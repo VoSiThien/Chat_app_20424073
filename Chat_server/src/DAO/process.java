6571b132b0d7b8442c48705d92c1e63a148f18aa
VoSiThien <vosithien1551999@gmail.com>
2021-06-08 20:56:36 +0700

    update file process

--- Chat_server/src/DAO/process.java
+++ Chat_server/src/DAO/process.java
@@ -3,12 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chat_server;
-
-/**
- *
- * @author Admin
- */
+package DAO;
+import java.util.*;
+import java.sql.*;
+import DB.DBConnection;
 public class process {
+    private DBConnection db = new DBConnection();
     
+    public int Login(String username, String password){
+        String sql = String.format("select * from users where username = '%s' and password = '%s'", username, password);
+        ResultSet rs  = db.loadDB(sql, db.con);
+        int check = 0;
+        try{
+            if(rs.next()){
+                check = 1;
+            }
+        }catch(SQLException ex){
+            System.out.println("Dang nhap loi!");
+        }
+        return check;
+    }
+    public int register(String username, String password){
+        String sqlkt = String.format("select * from users where username = '%s' and password = '%s'", username, password);
+        String sql = String.format("insert into users values('%s', '%s', %d)",username, password, 1);//0 la admin, 1 la user.
+        ResultSet rs  = db.loadDB(sql, db.con);
+        int check = 0;
+        try{
+            if(rs.next()){
+                check = -1;
+            }
+            else{
+                check = db.Add_Delete_Update(sql, db.con);
+            }
+        }catch(SQLException ex){
+            System.out.println("Dang ky loi!");
+        }
+        return check;
+    }
 }
--- Chat_server/src/DB/DBConnection.java
+++ Chat_server/src/DB/DBConnection.java
@@ -3,12 +3,63 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chat_server;
+package DB;
 
-/**
- *
- * @author Admin
- */
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import java.util.*;
+import java.sql.*;
+
 public class DBConnection {
-    
+    public Connection con = null;
+    public Connection connect() {
+
+        try {
+            String user = "sa";
+            String password = "";
+            String db = "qlchat";
+            int port = 1433;
+            SQLServerDataSource ds = new SQLServerDataSource();
+            ds.setUser(user);
+            ds.setPassword(password);
+            ds.setDatabaseName(db);
+            ds.setPortNumber(port);
+            con = ds.getConnection();
+            return con;
+            
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+        return null;
+    }
+    public int Add_Delete_Update(String sql, Connection connect)
+    {
+        if(connect !=null)
+        {
+            try {
+                Statement statement = connect.createStatement();
+                int rs = statement.executeUpdate(sql);
+                return rs;
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return -1;
+    }
+    public ResultSet loadDB(String sql, Connection connect)
+    {
+        ResultSet rs = null;
+        if(connect !=null) {
+            try {
+                Statement statement = connect.createStatement();
+                rs = statement.executeQuery(sql);
+
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            
+        }
+        return rs;
+    }
 }
